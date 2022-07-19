2d5463d1db39ab4c4ccedf8912161976780a79fe
vijayabharathy16 <vijayabharathy753@gmail.com>
2022-07-19 19:21:04 +0530

    initial

--- src/Main.jsx
+++ src/Main.jsx
@@ -17,7 +17,7 @@ function Main() {
                 <marquee direction="left" behavior="alternate" style={{color:"white"}}> Vijayabharathy</marquee>
               </p>
               <div className="d-flex justify-content-center mt-5">
-                <a href="https://drive.google.com/file/d/1u-08bwg3FCpdT-MXTWhrsjgrfQ6AuoxB/view?usp=sharing" target={"_blank"} className="btn btn-resume">
+                <a href="https://drive.google.com/file/d/1wz0LvduSVS7oKi9Qt5BypkjXjzwbxWRv/view?usp=sharing" target={"_blank"} className="btn btn-resume">
                 {/* <i className="fa fa-download"></i>  */}
                     View Resume</a>
                
